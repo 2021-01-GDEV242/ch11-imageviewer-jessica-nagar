26147a6f6cd9888f53aabaa2d1a9c072aa3bd071
jessica-nagar <77754279+jessica-nagar@users.noreply.github.com>
2021-04-19 11:54:45 -0400

    Update BlueTintFilter.java

--- BlueTintFilter.java
+++ BlueTintFilter.java
@@ -25,24 +25,30 @@ public class BlueTintFilter extends Filter
     {
         int height = image.getHeight();
         int width = image.getWidth();
+        Color blue = new Color (0, 0, 255); 
+        Color lightBlue = new Color (0, 0, 200);
+        Color medBlue = new Color (0, 0, 150);
+        Color darkBlue = new Color (0, 0, 100);
+        Color black = new Color(0, 0, 0);
+        
         for(int y = 0; y < height; y++) {
             for(int x = 0; x < width; x++) {
                 Color pixel = image.getPixel(x, y);
-                int brightness = (pixel.getBlue());
+                int brightness = (pixel.getRed());
                 if(brightness <= 85) {
-                    image.setPixel(x, y, Color.BLACK);
+                    image.setPixel(x, y, black);
                 }
                 else if(brightness <= 127) {
-                    image.setPixel(x, y, Color.DARK_GRAY);
+                    image.setPixel(x, y, darkBlue);
                 }
                 else if(brightness <= 170) {
-                    image.setPixel(x, y, Color.GRAY);
+                    image.setPixel(x, y, medBlue);
                 }
                 else if(brightness <= 212) {
-                    image.setPixel(x, y, Color.LIGHT_GRAY);
+                    image.setPixel(x, y, lightBlue);
                 }
                 else {
-                    image.setPixel(x, y, Color.WHITE);
+                    image.setPixel(x, y, blue);
                 }
             }
         }
